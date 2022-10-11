6b3855343176577db39dccd6ca02e3378555e18a
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-10-11 16:20:46 +0200

    first commit next.js app

---
+++ pages/Dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert(){
+    return(
+    <div>
+    <h1>Dessert</h1>
+    <p>bla bla bla</p> 
+    </div>
+    )
+ }
---
+++ pages/Veggie.js
@@ -0,0 +1,11 @@
+
+
+export default function Veggie(){
+   return(
+   <div>
+   <h1>Veggie</h1>
+   <p>bla bla bla</p> 
+   </div>
+   )
+}
+
